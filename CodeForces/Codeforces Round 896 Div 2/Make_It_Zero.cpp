b1c984518dcae33b5f387ade4f8920fbe8ffe11c
hiteshpal267 <75446182+hiteshpal267@users.noreply.github.com>
2023-10-05 05:01:43 +0530

    Create Make_It_Zero.cpp

---
+++ CodeForces/Codeforces Round 896 Div 2/Make_It_Zero.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 998244353
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        ll arr[n];
+
+        for(ll i = 0 ; i < n ; i++)
+        {
+            cin>>arr[i];
+        }
+
+        if(n%2 == 0)
+        {
+            cout<<"2"<<endl;
+            cout<<"1"<<" "<<n<<endl;
+            cout<<"1"<<" "<<n<<endl;
+        }
+        else{
+            cout<<"4"<<endl;
+            cout<<"1"<<" "<<n<<endl;
+            cout<<"1"<<" "<<n-1<<endl;
+            cout<<n-1<<" "<<n<<endl;
+            cout<<n-1<<" "<<n<<endl;
+        }
+    }
+    return 0;
+}
+
